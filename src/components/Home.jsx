3a393d7c8892550553fc11a409d73da14987e7cc
Nikhilrenjith <42229967+Nikhilrenjith@users.noreply.github.com>
2023-11-24 01:10:40 +0530

    Added External git link

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Profile from "./Pages/Profile";
 import Skills from "./Pages/Skills";
 import About from "./Pages/About";
+import Portfolio from "./Pages/Portfolio";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Profile />
       <Skills />
       <About />
+      <Portfolio />
     </>
   );
 };
---
+++ src/components/Pages/Portfolio.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import data from "../data/index.json";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import CallMadeIcon from "@mui/icons-material/CallMade";
+import LanguageIcon from "@mui/icons-material/Language";
+import "../../App.css";
+
+const Portfolio = (datas) => {
+  const handleClick = () => {
+    window.open("https://github.com/Nikhilrenjith", "_blank");
+  };
+  const links = () => {
+    console.log(datas.link);
+  };
+
+  return (
+    <section className="portfolio--section" id="MyPortfolio">
+      <div className="portfolio--container-box">
+        <div className="portfolio--container">
+          <p className="sub--title">Recent Projects</p>
+          <h2 className="section--heading">My Portfolio</h2>
+        </div>
+        <div>
+          <button className="btn btn-github" onClick={handleClick}>
+            <GitHubIcon />
+            My GitHub
+          </button>
+        </div>
+      </div>
+      <div className="portfolio--section--container">
+        {data?.portfolio?.map((item, index) => (
+          <div key={index} className="portfolio--section--card">
+            <div className="portfolio--section--img">
+              <img src={item.src} alt="Placeholder" />
+            </div>
+            <div className="portfolio--section--card--content">
+              <div>
+                <h3 className="portfolio--section--title">{item.title}</h3>
+                <p className="text-md">{item.description}</p>
+              </div>
+              <div className="card--button">
+                <button className="btn btn-github">
+                  {item.desc}
+                  <CallMadeIcon />
+                </button>
+                <button className="btn btn-github" onClick={links}>
+                  Website
+                  <LanguageIcon />
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Portfolio;
