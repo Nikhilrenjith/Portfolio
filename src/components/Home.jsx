4e305428638a94d2e76691258a763280c79edcb2
Nikhilrenjith <42229967+Nikhilrenjith@users.noreply.github.com>
2023-11-24 20:41:08 +0530

    Added Footer and contact

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,9 @@ import Profile from "./Pages/Profile";
 import Skills from "./Pages/Skills";
 import About from "./Pages/About";
 import Portfolio from "./Pages/Portfolio";
+import Review from "./Pages/Review";
+import Contact from "./Pages/Contact";
+import Footer from "./Pages/Footer";
 
 const Home = () => {
   return (
@@ -11,6 +14,9 @@ const Home = () => {
       <Skills />
       <About />
       <Portfolio />
+      <Review />
+      <Contact />
+      <Footer />
     </>
   );
 };
